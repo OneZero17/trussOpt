d7df9909f901cba330aa2aaf51740cf9740f7c07
hongjia_USFD <hongjialu18@gmail.com>
2019-08-07 11:30:22 +0100

    speeded up the collision check tool

--- tools/deleteOverlappingMembers.m
+++ tools/deleteOverlappingMembers.m
@@ -13,17 +13,53 @@ meshElements2 = [meshFacetNodes(:, [1 3 5 7]), meshFacetNodes(:, [2 4 6 8])];
 gElements = groundStructure.memberList;
 gElements = [gElements, zeros(size(gElements, 1), 1)];
 gElementNum = size(gElements, 1);
-meshElementsNode1 = meshElements2(:, 1:4);
-meshElementsNode2 = meshElements2(:, 5:8);
+meshElementsX = meshElements2(:, 1:4);
+meshElementsY = meshElements2(:, 5:8);
 existList = zeros(gElementNum, 1);
 memberXList = [gElements(:, 3), gElements(:, 5)];
 memberYList = [gElements(:, 4), gElements(:, 6)];
 
+gBoundXMin = min(memberXList, [], 2);
+gBoundXMax = max(memberXList, [], 2);
+gBoundYMin = min(memberYList, [], 2);
+gBoundYMax = max(memberYList, [], 2);
+
+mBoundXMin = min(meshElementsX, [], 2);
+mBoundXMax = max(meshElementsX, [], 2);
+mBoundYMin = min(meshElementsY, [], 2);
+mBoundYMax = max(meshElementsY, [], 2);
+mIndex = (1:elementNum)';
+
+contactMap = cell(gElementNum, 2);
 for i = 1:gElementNum
-        memberX = memberXList(i, :);
-        memberY = memberYList(i, :);
-    for j = 1:elementNum 
-        [xIntersect, yIntersect, ~, ~] = intersections(memberX, memberY, meshElementsNode1(j, :), meshElementsNode2(j, :), true);
+    contactMap{i, 2} = i;
+end
+
+for i = 1:gElementNum
+    contact = mIndex(mBoundXMin>=gBoundXMin(i) & mBoundXMax<=gBoundXMax(i)& mBoundYMin>=gBoundYMin(i)& mBoundYMax<=gBoundYMax(i));
+    if gBoundXMin(i) == gBoundXMax(i)
+        contact = mIndex(mBoundXMin<=gBoundXMin(i) & mBoundXMax>=gBoundXMax(i)& mBoundYMin>=gBoundYMin(i)& mBoundYMax<=gBoundYMax(i));
+    end
+    
+    if gBoundYMin(i) == gBoundYMax(i)
+        contact = mIndex(mBoundXMin>=gBoundXMin(i) & mBoundXMax<=gBoundXMax(i)& mBoundYMin<=gBoundYMin(i)& mBoundYMax>=gBoundYMax(i));
+    end
+    contactMap{i, 1} = contact;
+end
+
+contactMap = contactMap(~cellfun('isempty', contactMap(:, 1)), :);
+contactMemberNum = size(contactMap, 1);
+
+for i = 1:contactMemberNum
+        memberNum = contactMap{i, 2};
+        memberX = memberXList(memberNum, :);
+        memberY = memberYList(memberNum, :);
+        elementList = contactMap{i, 1};
+        contactElementNum = size(elementList, 1);
+        
+    for j = 1:contactElementNum 
+        elementNum = elementList(j, 1);
+        [xIntersect, yIntersect, ~, ~] = intersections(memberX, memberY, meshElementsX(elementNum, :), meshElementsY(elementNum, :), true);
         if (size(xIntersect, 1)~=0)
             if (size(xIntersect, 1) == 1)
                 distanceToNodeA = (xIntersect - memberX(1))^2 + (yIntersect - memberY(1))^2;
@@ -31,11 +67,11 @@ for i = 1:gElementNum
                 if distanceToNodeA < 1e-9 || distanceToNodeB < 1e-9
                     continue;
                 else
-                    existList(i, 1) = 1;
+                    existList(memberNum, 1) = 1;
                     break
                 end
             else
-                    existList(i, 1) = 1;
+                    existList(memberNum, 1) = 1;
                     break
             end
         end
